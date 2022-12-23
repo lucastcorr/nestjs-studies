8ef547fd7138be086660a1dc6c00eeacea97d66e
lucastcorr <lucastcs95@gmail.com>
2022-12-23 18:44:21 -0300

    create CatsService and interface

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
     @Get()
-    findAll(): string {
+    findAll(@Req() request: Request): string {
         return 'This action returns all cats';
+    }
+
+    @Post()
+    create(): string {
+        return 'This action adds a new cat'
     }
 }
