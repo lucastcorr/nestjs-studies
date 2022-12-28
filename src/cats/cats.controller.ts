a77457ccc4d97d4506f9feeea8dd1af027a08922
lucastcorr <lucastcs95@gmail.com>
2022-12-27 21:45:24 -0300

    create cats module

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,15 +1,19 @@
 import { Controller, Get, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { Cat } from './cats.interface';
+import { CatsService } from './cats.service';
 
 @Controller('cats')
 export class CatsController {
+    constructor(private readonly catsService: CatsService) {}
+
     @Get()
-    findAll(@Req() request: Request): string {
-        return 'This action returns all cats';
+    findAll(): Cat[] {
+        return this.catsService.findAll();
     }
 
     @Post()
-    create(): string {
-        return 'This action adds a new cat'
+    create(@Req() request: Request) {
+        return this.catsService.create(request.body);
     }
 }
